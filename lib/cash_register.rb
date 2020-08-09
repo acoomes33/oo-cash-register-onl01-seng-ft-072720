1e26483f4f852e7685d0eff7223b2d8649dde131
Alan Coomes <acoomes1@cord.edu>
2020-08-09 00:37:40 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -21,7 +21,7 @@ class CashRegister
   
   def apply_discount
     if @discount > 0
-      discounted_total = self.total *  (self.discount.to_f/100.0).to_i
+      discounted_total = total *  (self.discount.to_f/100.0).to_i
       binding.pry
        "After the discount, the total come to $#{discounted_total}."
     else 
